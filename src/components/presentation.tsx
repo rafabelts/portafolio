51d82c6d89bcdae53ce8b0832f954ace894dfa91
Rafael Beltran <rafabeltrans17@gmail.com>
2024-05-06 22:10:17 -0600

    Added new email

--- src/components/presentation.tsx
+++ src/components/presentation.tsx
@@ -24,7 +24,7 @@ class Presentation extends React.Component {
                     <h1 className="text-5xl lg:text-7xl mb-5 text-left lg:text-justify">Hi, <span className="text-greenPerry font-bold">Rafa</span> here!</h1>
                     <h2 className="text-lg lg:text-2xl">Full Stack Dev</h2>
                 </div>
-                <a href="mailto:rafabeltrans17@gmail.com" className="flex flex-row justify-start items-center gap-5 bg-container px-2 py-2 lg:px-4 lg:py-4 w-fit rounded-2xl hover:shadow-sm hover:shadow-white ">
+                <a href="mailto:rafaelbeltraans@gmail.com" className="flex flex-row justify-start items-center gap-5 bg-container px-2 py-2 lg:px-4 lg:py-4 w-fit rounded-2xl hover:shadow-sm hover:shadow-white ">
                     <MdEmail className="text-2xl lg:text-4xl" />
                     <p className="text-3xl lg:text-5xl"> Lets talk! </p>
                 </a>
